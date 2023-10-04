37c22873b71ac935efa52a2825d1e9f2e5f92c2a
Zenlexi <lang.kaelyn@gmail.com>
2023-10-04 10:50:36 -0500

    Calling pizza service

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppings() {
+    // Pretend to get pizza toppings from web service call
+    const toppings = [
+      {
+        name: 'Pepperoni',
+        price: 1.25
+      },
+      {
+        name: 'Sausage',
+        price: 2.50
+      },
+      {
+        name: 'Pineapple',
+        price: 3.75
+      }
+    ];
+
+    return toppings;
+  }
 }
