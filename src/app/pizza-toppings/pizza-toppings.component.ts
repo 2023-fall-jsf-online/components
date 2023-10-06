23e367938ec6ff34383158ce356a4acc9a065205
CDMcKinley <66882608+cdmckinley@users.noreply.github.com>
2023-10-06 11:19:24 -0500

    Implemented OnInit and injected PizzaService dependency

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
+
 
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
   styleUrls: ['./pizza-toppings.component.css']
 })
-export class PizzaToppingsComponent {
+export class PizzaToppingsComponent implements OnInit {
+
+  constructor(private pizzaService: PizzaService) {
+
+  }
+
+  ngOnInit(): void {
+    const pizzaToppings = this.pizzaService.getPizzaToppingsFromTheCloud();
+    
+  }
 
 }
