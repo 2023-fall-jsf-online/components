e177c7048b219e3b2e342e881e52961d4a53b3eb
CDMcKinley <66882608+cdmckinley@users.noreply.github.com>
2023-10-06 11:18:12 -0500

    Added function to get pizza toppings from hypothetical cloud For now, it always returns the same data.

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingsFromTheCloud() {
+    const toppingsFromWebService = [
+      {
+        name: 'Sausage'
+        , price: 1.75
+      }
+      , {
+        name: 'Pepperoni'
+        , price: 1.50
+      }
+      , {
+        name: 'Extra Cheese'
+        , price: 2.00
+      }
+    ];
+
+    return toppingsFromWebService;
+  }
 }
