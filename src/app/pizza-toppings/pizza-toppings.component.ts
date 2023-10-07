71ce55297b2940ad4bd5fb629faa5002bc027056
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2023-10-07 15:39:26 -0500

    added pizza toppings display interface . .
    and transformed the data to be the prope shape.

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -5,6 +5,12 @@ import { PizzaService } from '../pizza.service';
 //demo had import {Component, OnInit} and had the constructor and ngOnIT in there where mine d
 // did not have those items. 
 
+interface pizzaToppingDisplay {
+  name: string; 
+  price: number; 
+  checked: boolean;
+}
+
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
@@ -18,10 +24,21 @@ export class PizzaToppingsComponent implements OnInit {
     private pizzaSvc: PizzaService
   )  {}
 
+  availablePizzaToppings: pizzaToppingDisplay[] =[];
+
   ngOnInit(): void {
 
     const pt = this.pizzaSvc.getPizzaToppingsFromTheCloud(); 
     console.log(pt);
+
+    this.availablePizzaToppings = pt.map(
+      x => ({
+        ...x 
+        , checked: false
+      })
+    );
+
+    console.log(this.availablePizzaToppings);
   }
 
 }
