ee02e831ccfcc9c8a0a53d6dfd9a2bc6c3e48cb6
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2023-10-07 15:26:16 -0500

    Boom! We're calling a Di'd service!!
    This is fun - next lets show the pizza toppings in the component.

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
+//note in demo command was import component but then the
+// file in the demo here was different than mine ven though I created it the same - 
+//demo had import {Component, OnInit} and had the constructor and ngOnIT in there where mine d
+// did not have those items. 
 
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
   styleUrls: ['./pizza-toppings.component.css']
 })
-export class PizzaToppingsComponent {
+export class PizzaToppingsComponent implements OnInit {
+
+  // magic DI . . dependency injection. . 
+
+  constructor(
+    private pizzaSvc: PizzaService
+  )  {}
+
+  ngOnInit(): void {
+
+    const pt = this.pizzaSvc.getPizzaToppingsFromTheCloud(); 
+    console.log(pt);
+  }
 
 }
--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,25 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingsFromTheCloud() {
+    //pretend we make a web serice call. 
+    const toppingsFromWebService = [
+      {
+        name: 'Olives'
+        , price: 2.25
+      }
+
+      , {
+        name: 'Pepperoni'
+        , price: 1.50
+      }
+
+      , {
+        name: 'Sausage'
+        , price: 1.75
+      }
+    ];
+    return toppingsFromWebService;
+  }
 }
