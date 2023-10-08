f02966d6247986efbbcf982c10a046d3ccda8633
lavndor <143453791+vandoors@users.noreply.github.com>
2023-10-07 19:05:07 -0500

    Initialize PizzaService via dependency injection

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
   styleUrls: ['./pizza-toppings.component.css']
 })
-export class PizzaToppingsComponent {
+export class PizzaToppingsComponent implements OnInit {
+  // Dependency injection
+  constructor(private pizzaSvc: PizzaService) {}
 
+  ngOnInit(): void {
+    const pt = this.pizzaSvc.getPizzaToppingsViaCloud();
+    console.log(pt);
+  }
 }
--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingsViaCloud() {
+    // Imaginary web service call
+    const toppingsViaCloud = [
+      {
+        name: 'Olives',
+        price: 2.25
+      },
+      {
+        name: 'Pepperoni',
+        price: 1.25
+      },
+      {
+        name: 'Sausage',
+        price: 1.75
+      }
+    ];
+
+    return toppingsViaCloud;
+  }
 }
