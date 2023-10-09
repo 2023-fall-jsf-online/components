0f615145e4f273042f82f98ab2c64757a7f7a2f9
CDMcKinley <66882608+cdmckinley@users.noreply.github.com>
2023-10-09 16:07:34 -0500

    Made a PizzaGluer Compnent I forgot to make small commits, so here's the changes: imported and injected the ingredient service imported and implemented OnInit interface made a PizzaIngredientDisplay interface gets the ingredient list on OnInit modified the checkAll and uncheckAll functions to work with a new format added a getter for 'pizza', the ingredients glued together

--- src/app/pizza-gluer/pizza-gluer.component.ts
+++ src/app/pizza-gluer/pizza-gluer.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {IngredientServiceService} from '../ingredient-service.service'; // I forgot to leave off the word 'service' in generating the file :(
+
+interface PizzaIngredientDisplay {
+  name: string;
+  disabled: boolean
+  checked: boolean
+}
 
 @Component({
   selector: 'app-pizza-gluer',
   templateUrl: './pizza-gluer.component.html',
   styleUrls: ['./pizza-gluer.component.css']
 })
-export class PizzaGluerComponent {
+export class PizzaGluerComponent implements OnInit {
+  
+  // DI
+  constructor(
+    private ingredientSvc: IngredientServiceService // and the import name is messed up too :P
+  ) { }
 
+    availablePizzaIngredients: PizzaIngredientDisplay[] = []
+
+  ngOnInit(): void {
+    const pi = this.ingredientSvc.getIngredientsFromWebService(); // Pizza is pie and pizza is round. Appropriate name :)
+
+    this.availablePizzaIngredients = pi.map(
+      ingredient => ({
+        name: ingredient.name
+        , disabled: ingredient.required
+        , checked: ingredient.required ? true : false
+      })
+    );
+  }
+
+  get pizza() {
+    return this.availablePizzaIngredients
+        .filter(
+          x => x.checked
+        )
+        .join("+glue+")
+    ;
+  }
+
+  checkAll() {
+    this.availablePizzaIngredients.map(
+      x => ({
+        ...x,checked: true
+      })
+    );
+  }
+
+  uncheckAllOptional() {
+    this.availablePizzaIngredients.map(
+      x => ({
+        ...x
+        , checked: x.disabled ? true : false
+      })
+    )
+  }
 }
