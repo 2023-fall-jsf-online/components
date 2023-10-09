3167a06c21297efc9f799fc634d9ed8bfed207c0
CDMcKinley <66882608+cdmckinley@users.noreply.github.com>
2023-10-09 15:33:44 -0500

    Generated pizza-gluer component Gross!

---
+++ src/app/pizza-gluer/pizza-gluer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-gluer',
+  templateUrl: './pizza-gluer.component.html',
+  styleUrls: ['./pizza-gluer.component.css']
+})
+export class PizzaGluerComponent {
+
+}
