440bc799f14e9e79385607cec8cf3f88f86dd945
CDMcKinley <66882608+cdmckinley@users.noreply.github.com>
2023-10-09 15:45:32 -0500

    Made a function to 'get' pizza ingredients They're just literal objects though

--- src/app/ingredient-service.service.ts
+++ src/app/ingredient-service.service.ts
@@ -6,4 +6,35 @@ import { Injectable } from '@angular/core';
 export class IngredientServiceService {
 
   constructor() { }
+
+  getIngredientsFromWebService () {
+    const ingredientsFromWebService = [
+      {
+        name: 'Crust'
+        , required: true
+      }
+      , {
+        name: 'Marinara'
+        , required: true
+      }
+      , {
+        name: 'Cheese'
+        , required: true
+      }
+      , {
+        name: 'Olives'
+        , required: false
+      }
+      , {
+        name: 'Pepperoni'
+        , required: false
+      }
+      , {
+        name: 'Sausage'
+        , required: false
+      }
+    ];
+
+    return ingredientsFromWebService;
+  }
 }
