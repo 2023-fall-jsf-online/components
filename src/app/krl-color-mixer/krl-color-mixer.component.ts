6944873669ae158061ca6e8e3b123534c6fbc5f4
Zenlexi <lang.kaelyn@gmail.com>
2023-10-10 18:28:11 -0500

    Generated krl-color-mixer component

---
+++ src/app/krl-color-mixer/krl-color-mixer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-krl-color-mixer',
+  templateUrl: './krl-color-mixer.component.html',
+  styleUrls: ['./krl-color-mixer.component.css']
+})
+export class KrlColorMixerComponent {
+
+}
