56215f613796f0e88cfb06946dc3346e20c99cc5
Julia <143531581+JulCD@users.noreply.github.com>
2023-10-10 12:31:27 -0500

    Generated jcd-multiply component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { JcdMultiplyComponent } from './jcd-multiply/jcd-multiply.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    JcdMultiplyComponent
   ],
   imports: [
     BrowserModule
