848ed26e61a8826fb4e43e8b13377f51d4a3c017
Zenlexi <lang.kaelyn@gmail.com>
2023-10-10 18:56:36 -0500

    Added addRed method and button

--- src/app/krl-color-mixer/krl-color-mixer.component.ts
+++ src/app/krl-color-mixer/krl-color-mixer.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-krl-color-mixer',
   templateUrl: './krl-color-mixer.component.html',
   styleUrls: ['./krl-color-mixer.component.css']
 })
-export class KrlColorMixerComponent {
+export class KrlColorMixerComponent implements OnInit{
+
+  redValue : number = 0;
+  greenValue : number = 0;
+  blueValue : number = 0;
+
+  ngOnInit() : void {
+    
+  }
+
+  addRed() : void {
+    if (this.redValue >= 0 && this.redValue < 255) {
+      this.redValue += 5;
+      console.log(this.redValue);
+    }
+  }
 
 }
