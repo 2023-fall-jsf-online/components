95535197edf92d5cf53be856c482f637e0ebda75
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2023-10-15 09:20:43 -0500

    Generated Subtraction Component.

---
+++ src/app/ms-subtraction/ms-subtraction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ms-subtraction',
+  templateUrl: './ms-subtraction.component.html',
+  styleUrls: ['./ms-subtraction.component.css']
+})
+export class MsSubtractionComponent {
+
+}
