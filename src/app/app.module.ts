9cd5a5fe36959f76bc1c13d693c60473cc5ac86a
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2023-10-15 09:38:17 -0500

    added component
    added component to app.component.html
    added modules needed for new component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MsSubtractionComponent } from './ms-subtraction/ms-subtraction.component';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,6 +28,8 @@ import { MsSubtractionComponent } from './ms-subtraction/ms-subtraction.componen
     , MatCardModule
     , MatButtonModule
     , MatCheckboxModule
+    , MatInputModule
+    , MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
