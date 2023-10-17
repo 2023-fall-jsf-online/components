7a809cf6a237f0dca79d09072d40882ca3389da8
John Oliver <41064733+johnoliver42@users.noreply.github.com>
2023-10-16 19:05:34 -0500

    Added code to retrieve pizza toppings

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,13 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingsFromCloud() {
+    const toppingFromWebService = [
+      {name: 'Olives', price: 2.25}
+      , {name: 'Pepperoni', price: 1.50}
+      , {name: 'Sausage', price: 1.75}
+    ];
+    return toppingFromWebService;
+  }
 }
