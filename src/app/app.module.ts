c82cfccc010da8892fd9f538bf29af5571ac2686
lavndor <143453791+vandoors@users.noreply.github.com>
2023-10-19 14:37:20 -0500

    Generate jvd-image-picker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 import { PizzaGluerComponent } from './pizza-gluer/pizza-gluer.component';
+import { JvdImagePickerComponent } from './jvd-image-picker/jvd-image-picker.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,7 @@ import { PizzaGluerComponent } from './pizza-gluer/pizza-gluer.component';
     PizzaToppingsComponent,
     KrlColorMixerComponent
     , TjsMultiplicationComponent  
-    , PizzaGluerComponent
+    , PizzaGluerComponent, JvdImagePickerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/jvd-image-picker/jvd-image-picker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jvd-image-picker',
+  templateUrl: './jvd-image-picker.component.html',
+  styleUrls: ['./jvd-image-picker.component.css']
+})
+export class JvdImagePickerComponent {
+
+}
