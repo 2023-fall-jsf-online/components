6b6103e1f029705d2d677506a3d9c1e8700bbc2d
shabo3 <44040101+shabo3@users.noreply.github.com>
2023-10-21 15:55:00 -0400

    Generated my addition component...

---
+++ src/app/sjs-addition/sjs-addition.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SjsAdditionComponent } from './sjs-addition.component';
+
+describe('SjsAdditionComponent', () => {
+  let component: SjsAdditionComponent;
+  let fixture: ComponentFixture<SjsAdditionComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SjsAdditionComponent]
+    });
+    fixture = TestBed.createComponent(SjsAdditionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/sjs-addition/sjs-addition.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sjs-addition',
+  templateUrl: './sjs-addition.component.html',
+  styleUrls: ['./sjs-addition.component.css']
+})
+export class SjsAdditionComponent {
+
+}
