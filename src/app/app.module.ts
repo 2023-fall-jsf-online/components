08ea35fbdcbe9fa22dacd50fd96fe5f0e3cd1d56
shabo3 <44040101+shabo3@users.noreply.github.com>
2023-10-21 19:36:58 -0400

    wrong

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,39 +5,14 @@ import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-
-import { KrlColorMixerComponent } from './krl-color-mixer/krl-color-mixer.component';
-
-import { TjsMultiplicationComponent } from './tjs-multiplication/tjs-multiplication.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-
-import { PizzaGluerComponent } from './pizza-gluer/pizza-gluer.component';
-import { SjsAdditionComponent } from './sjs-addition/sjs-addition.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent,
-    KrlColorMixerComponent
-    , TjsMultiplicationComponent  
-    , PizzaGluerComponent
-    , SjsAdditionComponent
+    PizzaToppingsComponent
   ],
   imports: [
     BrowserModule
     , FormsModule
-    , BrowserAnimationsModule
-    , MatCardModule
-    , MatButtonModule
-    , MatCheckboxModule
-    , MatFormFieldModule
-    , MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
