23620e156f7192cf0c871f1abf1c607b58a68f53
shabo3 <44040101+shabo3@users.noreply.github.com>
2023-10-22 00:29:44 -0400

    Slack-n-tell done!!!
    Was as hard as i thought hahaha

--- src/app/sejs-addition/sejs-addition.component.ts
+++ src/app/sejs-addition/sejs-addition.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sejs-addition',
   templateUrl: './sejs-addition.component.html',
   styleUrls: ['./sejs-addition.component.css']
 })
-export class SejsAdditionComponent {
+export class SejsAdditionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  numberOne = 10;
+  numberTwo = 90;
+  total = 100;
+
+  add = () => this.total = this.numberOne + this.numberTwo
 
 }
