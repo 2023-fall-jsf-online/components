4130e425bea80c1ec92ec2576496fa0edbbe6b20
shabo3 <44040101+shabo3@users.noreply.github.com>
2023-10-21 23:27:25 -0400

    Start to build the addition component...
    remember to import the mat controls plan to use.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,18 @@ import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { SejsAdditionComponent } from './sejs-addition/sejs-addition.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,8 +26,15 @@ import { SejsAdditionComponent } from './sejs-addition/sejs-addition.component';
   imports: [
     BrowserModule
     , FormsModule
+    , BrowserAnimationsModule
+    , MatCardModule
+    , MatButtonModule
+    , MatCheckboxModule
+    , MatFormFieldModule
+    , MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
