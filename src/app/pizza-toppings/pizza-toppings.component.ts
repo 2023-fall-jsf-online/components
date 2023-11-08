daa0d2f08669591688c33ceb16d39425c32c4da9
bhietpas1 <bhietpas@madisoncollege.edu>
2023-11-08 17:50:42 -0600

    Calculating total correctly...
    Have seen map(), filter(), and reduce() in the real world...

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -36,4 +36,15 @@ export class PizzaToppingsComponent implements OnInit {
     console.log(this.availablePizzaToppings);
   }
 
+  totalPrice = 0;
+
+  caclculateTotal = () => {
+    this.totalPrice = this.availablePizzaToppings
+      .filter(x => x.checked)
+      .reduce(
+        (acc, x) => acc + x.price
+        , 0
+      )
+  }
+
 }
