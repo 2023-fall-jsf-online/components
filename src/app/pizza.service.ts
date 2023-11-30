feb2a9a8bcd9422f30b9eafeb3db7938c775be0d
jpdhmc <112502289+jpdhmc@users.noreply.github.com>
2023-11-30 00:08:08 -0600

    Calling DI'd service

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,19 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingsFromTheCloud() {
+    //make web service call here
+    const toppingsFromWebService = [
+      {
+        name: 'Olives',
+        price: 2.25
+      },
+      {
+        name: 'Pepperoni',
+        price: 1.75
+      }
+    ];
+    return toppingsFromWebService;
+  }
 }
