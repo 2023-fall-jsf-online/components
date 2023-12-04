dc6642d168997e591ee4457df29a73ace5761c5e
lparker7-matc <lparker7@madisoncollege.edu>
2023-12-03 19:28:49 -0600

    comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { LtpAdditionComponent } from './ltp-addition/ltp-addition.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    LtpAdditionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ltp-addition/ltp-addition.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ltp-addition',
+  templateUrl: './ltp-addition.component.html',
+  styleUrls: ['./ltp-addition.component.css']
+})
+export class LtpAdditionComponent {
+
+}
