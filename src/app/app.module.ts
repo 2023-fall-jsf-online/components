40009b299c219aa2597d99bbf0f5d8c4e0fa696e
lparker7-matc <lparker7@madisoncollege.edu>
2023-12-03 19:36:37 -0600

    building addition comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { LtpAdditionComponent } from './ltp-addition/ltp-addition.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -25,6 +27,8 @@ import { LtpAdditionComponent } from './ltp-addition/ltp-addition.component';
     , MatCardModule
     ,MatButtonModule
     ,MatCheckboxModule
+    ,MatInputModule
+    ,MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
